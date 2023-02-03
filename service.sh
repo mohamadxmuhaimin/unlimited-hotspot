112e57a86af32de7c0f645c5ab7165871408c725
nermur <29991266+felikcat@users.noreply.github.com>
2023-02-02 22:54:40 -0800

    v3
    - Fixed some props not setting correctly.
    - Various minor documentation/README improvements.

--- service.sh
+++ service.sh
@@ -1,6 +1,18 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
 
+# Don't automatically insert 'dun' into the APN,
+# which would persistently tell the telecom that tethering was used.
+# At that point, only after a reboot and not getting 'dun' added again would mask it.
+settings put global tether_dun_required 0
+
+# Tethering hardware acceleration causes high ping issues on the Pixel 4a (5G).
+settings put global tether_offload_disabled 1
+
+# Don't tell the telecom to check if tethering is even allowed for your data plan.
+settings put system net.tethering.noprovisioning true
+settings put system tether_entitlement_check_state 0
+
 # Failsafe: Incase these iptables entries were already present, remove them once.
 for INTERFACE in "v4-rmnet_data+" "wlan1" "bt-pan"; do
     iptables -t mangle -D PREROUTING -i $INTERFACE -j TTL --ttl-inc 1
